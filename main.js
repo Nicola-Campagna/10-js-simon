2d07ffd917bea3d59b956f30d9a98edea014b9be
Nicola-Campagna <campagnanicola379@gmail.com>
2023-05-23 12:58:54 +0200

    scheletro timer

---
+++ main.js
@@ -0,0 +1,11 @@
+// GIRONI
+const dayEl = document.getElementById("days");
+// ORA
+const hourEl = document.getElementById("hours");
+// MINUTI
+const minuteEl = document.getElementById("minutes");
+// SECONDI
+const secondEl = document.getElementById("seconds");
+
+
+
