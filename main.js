de1f61a8f8ce244723c5c2423482c83fe30e6659
Nicola-Campagna <campagnanicola379@gmail.com>
2023-05-25 11:16:06 +0200

    refactoring

--- main.js
+++ main.js
@@ -8,30 +8,55 @@ const minuteEl = document.getElementById("minutes");
 const secondEl = document.getElementById("seconds");
 
 
-setInterval(timer, 1000);
+
+timer();
+const intervalCountdown = setInterval((timer), 1000);
 
 
 
 function timer() {
-
     const now = new Date();
-    const tomorrow = new Date("2023-05-23 14:12");
-
-    const msNow = now.getTime();
-    const msTomorrow = tomorrow.getTime();
-
-    const msToTomorrow = msTomorrow - msNow;
-
-    const sToTomorrow = Math.floor(msToTomorrow / 1000);
-
-    const second = sToTomorrow % 60;
-    const minute = Math.floor(sToTomorrow / 60) % 60;
-    const hour = Math.floor(sToTomorrow / 60 / 60) % 24;
-    const day = Math.floor(sToTomorrow / 60 / 60 / 24);
+    const tomorrow = new Date("2023-12-24 00:01");
 
 
+    const [second, minute, hour, day] = countdown(now, tomorrow);
 
 
+    if (second <= 0 &&
+        minute <= 0 &&
+        hour <= 0 &&
+        day <= 0
+    ) {
+        clearInterval(intervalCountdown)
+    }
+
+    printTime(second, minute, hour, day);
+}
+
+
+function countdown(dateFrom, dateTarget) {
+
+    const msDateFrom = dateFrom.getTime();
+    const msDateTarget = dateTarget.getTime();
+
+    const msToDateTarget = msDateTarget - msDateFrom;
+
+    const sToDateTarget = Math.floor(msToDateTarget / 1000);
+
+    const second = sToDateTarget % 60;
+    const minute = Math.floor(sToDateTarget / 60) % 60;
+    const hour = Math.floor(sToDateTarget / 60 / 60) % 24;
+    const day = Math.floor(sToDateTarget / 60 / 60 / 24);
+
+    return [
+        second,
+        minute,
+        hour,
+        day
+    ]
+}
+
+function printTime(second, minute, hour, day) {
     dayEl.innerHTML = (day < 10) ? "0" + day : day;
     hourEl.innerHTML = (hour < 10) ? "0" + hour : hour;
     minuteEl.innerHTML = (minute < 10) ? "0" + minute : minute;
